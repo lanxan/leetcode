acc6fb26e1df76d692a94cae9ce186fe1880fe23
李翌辰 <lanxan0309@gmail.com>
2017-09-10 23:00:15 +0800

    Create subsets_v1.js

---
+++ subsets_v1.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+ 
+var subsets = function(nums) {
+    var ans = [];
+    ans.push([]);
+    if (nums.length == 0) {
+        return ans;
+    } else {
+        for(let subsum = 0; subsum < nums.length; subsum++) {
+            let tmp = [];
+            let top = subsum;
+            if (subsum == 0) {
+                for(let i = 0; i < nums.length; i++) {
+                    ans.push([nums[i]]);
+                }
+            } else {
+                nums.sort();
+                for(let i = 0; i < (nums.length - subsum); i++) {
+                    tmp = [];
+                    for(let x = i; x < top; x++) {
+                        tmp.push(nums[x]);
+                    }
+
+                    for(let j = top; j < nums.length; j++) {
+                        var subset = tmp.slice();
+                        subset.push(nums[j]);
+                        ans.push(subset);
+                    }
+                    top++;
+                }
+            }
+        }
+    }
+
+    return ans;
+};
